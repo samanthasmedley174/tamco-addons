7c8cfce37982bf5176d599b4e4e7e21384d8b3aa
samanthasmedley174 <samanthasmedley174@gmail.com>
2024-11-20 02:33:06 +0000

    updated item pricing data

--- TSCPriceFetcher/TSCPriceFetcher.lua
+++ TSCPriceFetcher/TSCPriceFetcher.lua
@@ -1,7 +1,7 @@
 -- TSCPriceFetcher.lua - Consolidated single file
 local TSC = {
     name = "TSCPriceFetcher",
-    version = 138
+    version = 137
 }
 
 -- Local references for performance
@@ -412,7 +412,7 @@ local function setupSettingsMenu()
     local whatsNewButton = {
         type = LHAS.ST_BUTTON,
         label = "What's New",
-        tooltip = [[v138: Updated with new sales data for week starting 2025-10-28
+        tooltip = [[v137: Updated with new sales data for week starting 2025-10-21
 
 Scan the QR code to view full update details]],
         buttonText = "View Update Info",
